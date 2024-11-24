01b3d6d70948720795811c9656f42ff2f955cbea
Kennedysenyo <kennedysenyo@gmail.com>
2024-11-24 09:51:40 +0000

    Work on gradient background, responsiveness, error handling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import {config} from "dotenv";
 
 const app = express();
 const port = 3000;
@@ -11,17 +12,68 @@ const freeIpAPI_URL = "https://freeipapi.com/api/json/"
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+config();
 
 // Use OpenWeather API to get current Weather from a specified longitude and Latitiude.
 // Open Weather - https://openweathermap.org/
 const openWeatherAPI_URL = "https://api.openweathermap.org/data/2.5/weather";
-const openWeatherAPI_KEY = "c4cdceee34a78089d0ee144694bce6a2"
+const openWeatherAPI_KEY = process.env.OPEN_WEATHER_API_KEY;
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get(freeIpAPI_URL);
-    const result = response.data;
+    const result = response.data;  
     const lat = result.latitude;
     const lon = result.longitude;
+
+    try {
+      const weatherResponse = await axios.get(
+        `${openWeatherAPI_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${openWeatherAPI_KEY}`
+      );
+
+      // Use `weatherResponse.data` instead of `JSON.stringify` to keep it as an object
+      const weatherData = weatherResponse.data;
+
+      const icon = weatherData.weather[0].icon; 
+
+      const temp = Math.round(weatherData.main.temp);
+
+      const address = weatherData.name; 
+
+      const humi = weatherData.main.humidity;
+    
+      const wind = weatherData.wind.speed;
+
+      res.render("index.ejs", {
+        _icon: `icons/${icon}.png`,
+        _temp: temp,
+        _address: address,
+        _humi: humi,
+        _wind: wind,
+      });
+    } catch (error) {
+      res.status(500).render("error.ejs", {
+        _message: "Something went wrong!",
+      })
+    }
+  } catch (error) {
+    console.error("Failed to make request", error.message);
+  }
+});
+
+
+// Use this API to get geo-coordinates from human readable address.
+// Geocoding API - https://geocode.maps.co/
+const geocodingAPI_URL = "https://geocode.maps.co/search?";
+const geocodingAPI_Key = process.env.GEOCODING_API_KEY;
+
+app.post("/search", async (req, res) => {
+  const address = req.body.address;
+  console.log(req.body.address);
+  try {
+    const response = await axios.get(`${geocodingAPI_URL}q=${address}&api_key=${geocodingAPI_Key}`);
+    const result = response.data;
+    const lat = result[0].lat;
+    const lon = result[0].lon;
 
     try {
       const weatherResponse = await axios.get(
@@ -53,17 +105,11 @@ app.get("/", async (req, res) => {
     }
   } catch (error) {
     console.error("Failed to make request", error.message);
+    res.render("index.ejs", {
+      _error: "Search Invalid! Search town, city, or country.",
+    });
   }
-});
-
-
-// Use this API to get geo-coordinates from human readable address.
-// Geocoding API - https://geocode.maps.co/
-const geocodingAPI_URL = "https://geocode.maps.co/search?";
-const geocodingAPI_Key = "674147f957fe9461980883awbeb5232";
-
-app.post("/search", (req, res) => {
-
+  
 });
 
 app.listen(port, () => {
